b022071faa2f444a4085ef895f573a76de451060
yuniamilatina <112361106+yuniamilatina@users.noreply.github.com>
2023-01-24 10:00:44 +0700

    Add files via upload

--- application/views/aorta/download_spkl/excel.php
+++ application/views/aorta/download_spkl/excel.php
@@ -1,1 +1,25 @@
+<?php
+    header ("Cache-Control: no-cache, must-revalidate");
+    header ("Pragma: no-cache");
+    header ("Content-type: application/x-msexcel");
+    header ("Content-type: application/octet-stream");
+    header ("Content-Disposition: attachment; filename=Name_file.xls");
+?>
 
+<?php
+if ($data->num_rows() >0) {
+    foreach ($data->result() as $row ){
+        ?>
+        <tr>
+            <td> <?php echo $row-> ; ?></td>
+            <td> <?php echo $row->; ?></td>
+            <td> <?php echo $row->; ?></td>
+            <td> <?php echo $row->; ?></td>
+            <td> <?php echo $row->; ?></td>
+            <td> <?php echo $row-> ; ?></td>
+            <td> <?php echo $row-> ; ?></td>
+            <td> <?php echo $row->; ?></td>
+            <td> <?php echo $row-> remark; ?></td>
+            
+    }
+}
